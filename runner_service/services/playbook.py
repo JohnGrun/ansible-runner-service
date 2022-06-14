9713753cc5a1d09c0805a3d51b87a04eaa63e847
john <john.grun@clearme.com>
2022-06-14 13:07:15 -0400

    attempted hack to fix inventory issue.

--- runner_service/services/playbook.py
+++ runner_service/services/playbook.py
@@ -213,7 +213,7 @@ def start_playbook(playbook_name, vars=None, filter=None, tags=None):
     parms = {
         "private_data_dir": private_data_dir,
         "project_dir": os.path.join(configuration.settings.playbooks_root_dir, 'project'),
-        "inventory": os.path.join(configuration.settings.playbooks_root_dir, 'inventory'),
+        #"inventory": os.path.join(configuration.settings.playbooks_root_dir, 'inventory'),
         "artifact_dir": artifacts_dir,
         "settings": settings,
         "finished_callback": cb_playbook_finished,
@@ -250,6 +250,12 @@ def start_playbook(playbook_name, vars=None, filter=None, tags=None):
     logger.debug("Run the playbook with a private key override of "
                  "{}".format(configuration.settings.ssh_private_key))
     cmdline.append("--private-key {}".format(configuration.settings.ssh_private_key))
+
+    inventory_path = os.path.join(configuration.settings.playbooks_root_dir, 'inventory/hosts.yml')
+    logger.debug("Run the playbook with an Inventory override of "
+                 "{}".format(inventory_path))
+
+    cmdline.append("--inventory {}".format(inventory_path))
 
     parms['cmdline'] = ' '.join(cmdline)
 
